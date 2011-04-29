26bd056da269b682be2efac94630cf7ea13a1000
Nathan Hamblen <nathan@technically.us>
2011-04-29 09:15:00 +0200

    close stream

--- src/main/scala/produce.scala
+++ src/main/scala/produce.scala
@@ -9,6 +9,7 @@ object Produce {
     pages.foreach { page =>
       val out = new FileOutputStream(new File(target, Printer.webify(page.name)))
       out.write(printer.print(page).toString.getBytes("utf-8"))
+      out.close()
     }
   }
 }
