772995dbdf1359c7d71e0b33c36643b2ca847618
Nathan Hamblen <nathan@technically.us>
2011-06-25 18:55:55 -0400

    sbt 0.10 build

---
+++ build.sbt
@@ -0,0 +1,5 @@
+organization := "net.databinder"
+
+name := "pamflet project"
+
+version := "0.2.1"
--- project/plugins/plugins.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val pf = "net.databinder" % "pamflet-plugin" % "0.1.3"
-  val conscript = "net.databinder" % "conscript-plugin" % "0.2.2"
-}
