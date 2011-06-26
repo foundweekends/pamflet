3152f9aba5c61e79cae4da4cacc4fd7bc9ba579b
Nathan Hamblen <nathan@technically.us>
2011-06-26 19:11:20 -0400

    also need to publish stuff

--- project/build.scala
+++ project/build.scala
@@ -5,7 +5,10 @@ object PamfletBuild extends Build {
   lazy val common = Defaults.defaultSettings ++ Seq(
     organization := "net.databinder",
     version := "0.2.2",
-    crossScalaVersions := Seq("2.8.1")
+    crossScalaVersions := Seq("2.8.1"),
+    publishTo := Some("Scala Tools Nexus" at 
+      "http://nexus.scala-tools.org/content/repositories/releases/"),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
   )
 
   lazy val pamflet: Project =
