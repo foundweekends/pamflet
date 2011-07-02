47ad397fba4406a4b449e88880916fa654cc84b5
Nathan Hamblen <nathan@technically.us>
2011-07-02 15:42:19 -0400

    some swipe tracking

--- library/src/main/resources/webroot/js/pamflet.js
+++ library/src/main/resources/webroot/js/pamflet.js
@@ -17,6 +17,25 @@ $(function() {
             next();
         }
     });
-    $(document).bind("swiperight", prev);
-    $(document).bind("swipeleft", next);
+    container = $(".container")[0];
+    var orig = null;
+    container.ontouchstart = function(e){
+        if(e.touches.length == 1 && e.changedTouches.length == 1){
+            orig = e.touches[0];
+        } else orig = null;
+    };
+    var horiz = function(touch) {
+        return orig && Math.abs(
+            (touch.clientY - orig.clientY) / (touch.clientX - orig.clientX)
+        ) < 1;
+    }
+    container.ontouchmove = function(e){
+        if(e.touches.length == 1){
+            var touch = e.touches[0];
+            if (horiz(touch)) {
+                e.preventDefault();
+                container.style.left = (touch.clientX - orig.clientX) + "px";
+            }
+        }
+    };
 });
