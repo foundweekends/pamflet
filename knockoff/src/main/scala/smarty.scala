41abc0201f2e40c0e7beae0e1c5f0b8af9b92bb8
Nathan Hamblen <nathan@technically.us>
2011-07-01 22:10:09 -0400

    a fair bit of smart quoting

--- knockoff/src/main/scala/smarty.scala
+++ knockoff/src/main/scala/smarty.scala
@@ -2,6 +2,7 @@ package pamflet
 
 import com.tristanhunt.knockoff._
 import scala.util.parsing.input.{ CharSequenceReader, Position, Reader }
+import scala.util.matching.Regex
 
 trait SmartyDiscounter extends Discounter {
   override def createSpanConverter(
@@ -10,17 +11,31 @@ trait SmartyDiscounter extends Discounter {
 }
 
 trait SmartySpanConverter extends SpanConverter {
+  val punctClass = """[!\"#\$\%'()*+,-.\/:;<=>?\@\[\\\]\^_`{|}~]"""
+  val closeClass = """[^\ \t\r\n\[\{\(\-]"""
   def smartyMatchers : List[ String => Option[SpanMatch] ] = List(
-    matchOpenSQuote
+    // special case for quote as first character, then punctuation
+    replacer("""^'(?=%s\B)""".format(punctClass).r, "‘"),
+    replacer("""^"(?=%s\B)""".format(punctClass).r, "“"),
+    // normal opening quote cases
+    replacer("""(?<=\s|--|—)'(?=\w)""".r, "‘"),
+    replacer("""(?<=\s|--|—)"(?=\w)""".r, "“"),
+    // closing quotes
+    replacer("""(?<=%s)?'(?=\s|\w|%s)""".format(closeClass, punctClass).r, "’"),
+    replacer("""(?<=%s)?"(?=\s|%s)""".format(closeClass, punctClass).r, "”"),
+    // assume everything else is opening
+    replacer("'".r, "‘"),
+    replacer("\"".r, "“"),
+    replacer("--".r, "—"),
+    replacer("""\.\.\.""".r, "…")
   )
   override def matchers =  smartyMatchers ::: super.matchers
 
-  private val matchOpenSQuoteRE = """'(\w+)""".r
-  
-  def matchOpenSQuote( source : String ) : Option[ SpanMatch ] =
-    matchOpenSQuoteRE.findFirstMatchIn( source ).map { qmatch =>
+  private def replacer(r: Regex, smarted: String)(source : String) = {
+    r.findFirstMatchIn( source ).map { qmatch =>
       val before = qmatch.before.toOption.map( Text(_) )
-      val html = HTMLSpan( "‘" + qmatch.group(1) )
+      val html = HTMLSpan(smarted)
       SpanMatch( qmatch.start, before, html, qmatch.after.toOption )
     }
+  }
 }
