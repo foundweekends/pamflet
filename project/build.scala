0fe3d6f566109f2af1dd74629365a0207125077b
Nathan Hamblen <nathan@technically.us>
2012-01-06 00:03:49 -0500

    ls metadata

--- project/build.scala
+++ project/build.scala
@@ -2,9 +2,9 @@ import sbt._
 import Keys._
 
 object PamfletBuild extends Build {
-  lazy val common = Defaults.defaultSettings ++ Seq(
+  lazy val common = Defaults.defaultSettings ++ ls.Plugin.lsSettings ++ Seq(
     organization := "net.databinder",
-    version := "0.3.1-SNAPSHOT",
+    version := "0.3.1",
     scalaVersion := "2.9.1",
     publishTo := Some("Scala Tools Nexus" at 
       "http://nexus.scala-tools.org/content/repositories/releases/"),
@@ -13,15 +13,23 @@ object PamfletBuild extends Build {
 
   lazy val pamflet: Project =
     Project("pamflet", file("."), 
-            settings = common) aggregate(knockoff, library, app)
+            settings = common ++ Seq(
+              ls.Plugin.LsKeys.skipWrite := true
+            )) aggregate(knockoff, library, app)
   lazy val knockoff: Project =
     Project("pamflet-knockoff", file("knockoff"),
-            settings = common)
+            settings = common ++ Seq(
+              description := "Extensions to the Knockoff Markdown parser"
+            ))
   lazy val library: Project =
     Project("pamflet-library", file("library"),
-            settings = common) dependsOn knockoff
+            settings = common ++ Seq(
+              description := "Core Pamflet library"
+            )) dependsOn knockoff
   lazy val app: Project =
     Project("pamflet-app", file("app"),
-            settings = common ++ conscript.Harness.conscriptSettings
+            settings = common ++ conscript.Harness.conscriptSettings ++ Seq(
+              description := "Pamflet app for previewing and publishing"
+            )
     ) dependsOn library
 }
