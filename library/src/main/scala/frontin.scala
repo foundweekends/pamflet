6c15bade7af1b8174c38867fbe7a9292e890f6b6
e.e d3si9n <eed3si9n@gmail.com>
2012-02-13 01:14:33 -0500

    keep the original endline chars

--- library/src/main/scala/frontin.scala
+++ library/src/main/scala/frontin.scala
@@ -9,12 +9,12 @@ object Frontin {
     (str.trim == seperator) && (str startsWith seperator)
   
   def apply(str: String): Frontin =
-    str.lines.toList match {
+    str.linesWithSeparators.toList match {
       case Nil => Frontin(None, "")
       case x :: xs if seperates(x) =>
         xs span {!seperates(_)} match {
-          case (h, b) => Frontin(Some(h.mkString("\n")),
-            if (b isEmpty) "" else b.tail.mkString("\n"))
+          case (h, b) => Frontin(Some(h.mkString("")),
+            if (b isEmpty) "" else b.tail.mkString(""))
         }
       case _ => Frontin(None, str)
     }
