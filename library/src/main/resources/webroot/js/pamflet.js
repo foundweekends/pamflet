aefc11df9fc0bd3a16e6a1a496a07abb0358d90f
Nathan Hamblen <nathan@technically.us>
2013-05-07 22:49:43 -0400

    switch to present (mobile) with ctrl+f5

--- library/src/main/resources/webroot/js/pamflet.js
+++ library/src/main/resources/webroot/js/pamflet.js
@@ -1,6 +1,6 @@
 $(function() {
     var load = function() {
-        window.location = this.href;
+        window.location = this.href + document.location.search;
     };
     var prev = function() {
         $("a.page.prev").first().each(load);
@@ -8,12 +8,25 @@ $(function() {
     var next = function() {
         $("a.page.next").first().each(load);
     }
-    $(document).keyup(function (event) {
-        if (event.altKey || event.ctrlKey || event.shiftKey || event.metaKey)
+    var present = function() {
+        for (i in document.styleSheets) {
+            var sheet = document.styleSheets[i];
+            if (sheet.media && sheet.media.mediaText.indexOf("min-device-width") > -1)
+                sheet.disabled = true;
+        }
+    }
+    if (window.location.search.indexOf("present") > -1) {
+        present();
+    }
+    $(document).keydown(function (event) {
+        if (event.ctrlKey && event.which == 116) {
+            event.preventDefault();
+            window.location = window.location + "?present";
+        } else if (event.altKey || event.ctrlKey || event.shiftKey || event.metaKey) {
             return;
-        if (event.keyCode == 37) {
+        } else if (event.which == 37) {
             prev();
-        } else if (event.keyCode == 39) {
+        } else if (event.which == 39) {
             next();
         }
     });
