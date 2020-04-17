ac66c80ffb470e602b0e3bfb70ad5f0e6c8ce652
Josh Soref <jsoref@users.noreply.github.com>
2020-04-17 14:11:16 -0400

    spelling: separates

--- library/src/main/scala/frontin.scala
+++ library/src/main/scala/frontin.scala
@@ -5,14 +5,14 @@ case class Frontin(header: Option[String], body: String)
 object Frontin {
   val seperator = "---"
 
-  def seperates(str: String): Boolean =
+  def separates(str: String): Boolean =
     (str.trim == seperator) && (str startsWith seperator)
   
   def apply(str: String): Frontin =
     str.linesWithSeparators.toList match {
       case Nil => Frontin(None, "")
-      case x :: xs if seperates(x) =>
-        xs span {!seperates(_)} match {
+      case x :: xs if separates(x) =>
+        xs span {!separates(_)} match {
           case (h, b) => Frontin(Some(h.mkString("")),
             if (b isEmpty) "" else b.tail.mkString(""))
         }
